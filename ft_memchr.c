1c48f735102211f2588fe3245cc16cf4e819e435
tarcay <tarcay@student.42.fr>
2020-12-30 22:13:08 +0100

    modification de ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: tarcay <tarcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 11:37:44 by tarcay            #+#    #+#             */
-/*   Updated: 2020/12/30 22:06:34 by tarcay           ###   ########.fr       */
+/*   Updated: 2020/12/30 22:12:45 by tarcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,11 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	char	*str;
-	int		i;
-
-	str = (char *)s;
-	i = 0;
-	while (i < (int)n)
+	while (n--)
 	{
-		if (str[i] == (unsigned char)c)
-			return ((unsigned char)(str + i));
-		i++;
+		if ((unsigned char *)s == (unsigned char)c)
+			return ((unsigned char *)s);
+		s++;
 	}
 	return (NULL);
 }
