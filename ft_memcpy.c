08e501954374eb5257ec1c7e30819cacabdc787d
tarcay <tarcay@student.42.fr>
2020-12-30 21:55:54 +0100

    modification de ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tarcay <tarcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 11:42:34 by tarcay            #+#    #+#             */
-/*   Updated: 2020/12/30 21:45:38 by tarcay           ###   ########.fr       */
+/*   Updated: 2020/12/30 21:54:26 by tarcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char		*dest_mem;
 	const char	*src_mem;
 
-	if(n < 1)
-		return (NULL);
+	if(!dest && !src)
+		return (dest);
 
 	dest_mem = dest;
 	src_mem = src;
