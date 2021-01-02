72475e0e3e66aa194be76bc1e01c315b6ef5e2ff
tarcay <tarcay@student.42.fr>
2021-01-02 21:26:54 +0100

    modification de ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,24 @@
 /*   By: tarcay <tarcay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/20 11:59:14 by tarcay            #+#    #+#             */
-/*   Updated: 2020/12/20 12:01:18 by tarcay           ###   ########.fr       */
+/*   Updated: 2021/01/02 21:26:16 by tarcay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(int nb, int fd)
 {
-	char	*str;
+	unsigned int	nbr;
 
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
+	if (nb < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr = (unsigned int)(nb * -1);
+	}
+	else
+		nbr = (unsigned int)nb;
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd((char)(nbr % 10 + 48), fd);
 }
